4c95937e9f64ff8f54dfadf6d6ef31231fb04b1f
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2026-04-12 22:12:46 +0200

    add takeUntilDestroyed check

--- src/rules/no-inject-outside-di-context.ts
+++ src/rules/no-inject-outside-di-context.ts
@@ -10,11 +10,14 @@ import { isInRoute } from "../utils/angular-route";
 
 export const ruleName = "no-inject-outside-di-context";
 
-export const INJECT_DOC = "https://angular.dev/api/core/inject";
-export const DEPENDENCY_INJECTION_CONTEXT_DOC =
-  "https://angular.dev/guide/di/dependency-injection-context";
+const DEPENDENCY_INJECTION_CONTEXT_DOC = "https://angular.dev/guide/di/dependency-injection-context";
+const noInjectOutsideInjectionContextMessageId = "noInjectOutsideInjectionContext";
+const noTakeUntilDestroyedWithoutInjectionContextMessageId = "noTakeUntilDestroyedWithoutInjectionContext";
 
-function isInjectInInjectionContext(node: TSESTree.Node): boolean {
+function isInInjectionContext(node: TSESTree.Node, {
+  includeFactories = false,
+  includeAppInitializationFunctions = false,
+} = {}): boolean {
   const parent: TSESTree.Node | undefined = node.parent;
 
   if (
@@ -23,13 +26,13 @@ function isInjectInInjectionContext(node: TSESTree.Node): boolean {
     (isInAngularClassInitialization(parent) ||
       // Special contexts (guard, resolver and interceptor) are the second most common case
       // 1. modern function syntax, 2. legacy class syntax, 3. directly inline inside a route
-      isInFunctionTypeWithInjectionContext(parent, { includeAppInitializationFunctions: true }) ||
+      isInFunctionTypeWithInjectionContext(parent, { includeAppInitializationFunctions }) ||
       isInMethodWithInjectionContext(parent) ||
       isInRoute(parent) ||
       // Factories
-      isInFactoryFunction(parent) ||
+      (includeFactories && isInFactoryFunction(parent)) ||
       // Special functions like `runInInjectionContext` and some application providers
-      isInFunctionWithInjectionContext(parent, { includeAppInitializationFunctions: true }) ||
+      isInFunctionWithInjectionContext(parent, { includeAppInitializationFunctions }) ||
       // Custom injectable functions where context is asserted
       isInjectionContextAsserted(parent))
   ) {
@@ -43,10 +46,11 @@ export const ruleDefinition: RuleDefinition = {
   meta: {
     type: "problem",
     messages: {
-      noInjectOutsideDiContext: `\`inject()\` must be called in an injection context. See more at ${INJECT_DOC} and ${DEPENDENCY_INJECTION_CONTEXT_DOC}`,
+      [noInjectOutsideInjectionContextMessageId]: `\`inject()\` must be called in an injection context. See more at https://angular.dev/api/core/inject and ${DEPENDENCY_INJECTION_CONTEXT_DOC}`,
+      [noTakeUntilDestroyedWithoutInjectionContextMessageId]: `\`takeUntilDestroyed()\` must be called in an injection context, or a \`DestroyRef\` must be provided as the first argument. Documentation: https://angular.dev/api/core/rxjs-interop/takeUntilDestroyed, https://angular.dev/api/core/rxjs-interop/takeUntilDestroyed and ${DEPENDENCY_INJECTION_CONTEXT_DOC}`,
     },
     docs: {
-      description: `Ensures that \`inject()\` is called in an injection context`,
+      description: `Checks that functions requiring an injection context are indeed called in an injection context, or are called with an explicit injection context as an argument.`,
       recommended: true,
     },
     schema: [],
@@ -54,18 +58,31 @@ export const ruleDefinition: RuleDefinition = {
   create(context) {
     return {
       CallExpression(node: TSESTree.CallExpression) {
-        if (
-          node.callee.type !== AST_NODE_TYPES.Identifier ||
-          node.callee.name !== "inject" ||
-          isInjectInInjectionContext(node)
-        ) {
+        if (node.callee.type !== AST_NODE_TYPES.Identifier) {
           return;
         }
 
-        context.report({
-          node,
-          messageId: "noInjectOutsideDiContext",
-        });
+        switch (node.callee.name) {
+          case "inject": {
+            if (!isInInjectionContext(node, { includeFactories: true, includeAppInitializationFunctions: true })) {
+              context.report({
+                node,
+                messageId: noInjectOutsideInjectionContextMessageId,
+              });
+            }
+            break;
+          }
+          case "takeUntilDestroyed": {
+            /* Takes a `DestroyRef` as first argument: `takeUntilDestroyed(this.destroyRef)` */
+            if (node.arguments.length === 0 && !isInInjectionContext(node)) {
+              context.report({
+                node,
+                messageId: noTakeUntilDestroyedWithoutInjectionContextMessageId,
+              });
+            }
+            break;
+          }
+        }
       },
     };
   },
